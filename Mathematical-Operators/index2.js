393823d82a56850656e671173c8c2ec1a9065c07
DhaanuI <dhaanu97@gmail.com>
2022-09-04 22:14:43 +0530

    Mathematical-Operators

---
+++ Mathematical-Operators/index2.js
@@ -0,0 +1,11 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+
+// two five six are doubled
+// three four are tripled
+// one unchanged
+console.log(one + two * 2 + five * 2 + six * 2 + three * 3 + four * 3)
